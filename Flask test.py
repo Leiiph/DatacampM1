f3abc9dcb4666481703e24516a56c189c59a3d2d
Leiiph <60736065+Leiiph@users.noreply.github.com>
2023-10-30 01:31:15 +0100

    aled²

--- Flask test.py
+++ Flask test.py
@@ -5,14 +5,10 @@ import pandas as pd
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras import models
 from tensorflow.keras import layers
-import matplotlib.pyplot as plt
 import keras
 import transformers
 import sentencepiece
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.utils import to_categorical
-import sentencepiece as spm
-from transformers import T5Tokenizer
 import sentencepiece as spm
 from transformers import T5Tokenizer
 
@@ -49,9 +45,15 @@ def recommendation(input):
     return predicted_genre
 
 def rec_music(de, predicted_genre):
-    grouped_de = de.groupby(predicted_genre)
-    top_tracks = grouped_de[['artist_name', 'track_name']].head(5)
-    return top_tracks
+    stock = de[de['topic'] == predicted_genre]
+    if stock.empty:
+        return "Sorry, we don't have any recommendation for this genre yet."
+    else:
+        stock = stock.sample(n=3)
+        text = ""
+        for i in range(len(stock)):
+            text += str(stock.iloc[i]['artist_name']).title() + " - " + str(stock.iloc[i]['track_name']).title() + " | "
+        return text
 
 @app.route('/process_text', methods=['POST'])
 def process_text():
@@ -59,34 +61,34 @@ def process_text():
     user_input = request.form.get('user_input')
     singer_name = request.form.get('singer_name')
     song_name = request.form.get('song_name')
+    # Lower all letters (as the database uses only lower case)
+    singer_name = singer_name.lower()
+    song_name = song_name.lower()
 
     show_lyric_input = True  # Assume lyric input is shown by default
     lyric_input_invalid = False  # Assume lyric input is valid
 
     if singer_name and song_name:
-        # Check if singer_name and song_name exist in the DataFrame
         song_info = de[(de['artist_name'] == singer_name) & (de['track_name'] == song_name)]
 
         if not song_info.empty:
-            # Use the lyrics from the DataFrame if the song info exists
-            lyrics = song_info['lyrics'].values[0]
-            show_lyric_input = False  # Don't show lyric input
+            lyrics_exist = True
+            show_lyric_input = False 
         else:
-            # Prompt the user to enter lyrics
+            # User enter lyrics
+            lyrics_exist = False
             lyrics = user_input
             lyric_input_invalid = True
     else:
-        # If either singer_name or song_name is missing, use the user's input
         lyrics = user_input
 
     # The rest of your code to process the lyrics and provide recommendations
+    if lyrics_exist == False:
+        predicted_genre = recommendation(lyrics)
+    else:
+        predicted_genre = song_info['topic'].values[0]
 
-    predicted_genre = recommendation(lyrics)
-
-    return render_template('result_template.html', user_input=user_input, predicted_genre=predicted_genre, rec_music=rec_music(de, predicted_genre), show_lyric_input=show_lyric_input, lyric_input_invalid=lyric_input_invalid)
+    return render_template('result_template.html', predicted_genre=str(predicted_genre).title(), rec_music=rec_music(de, predicted_genre), show_lyric_input=show_lyric_input, lyric_input_invalid=lyric_input_invalid)
 
 if __name__ == '__main__':
- 
-    # run() method of Flask class runs the application 
-    # on the local development server.
     app.run()
